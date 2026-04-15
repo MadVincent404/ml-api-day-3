0f93ba275933df0eb194c04a0fb6c5d935a87c1f
unknown <lebrennv@gmail.com>
2026-04-16 01:35:39 +0200

    Patch python env

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-from model import model_service
-from schemas import IrisFeatures, PredictionResponse, BatchRequest
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from jour_3.app.main import app
 
 client = TestClient(app)
 
